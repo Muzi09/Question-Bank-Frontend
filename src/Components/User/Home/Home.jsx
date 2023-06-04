897cc8508618731006c1bc8ce49e6de9083947e7
Muzi09 <amzikhan711@gmail.com>
2023-06-04 14:18:19 +0530

    Minor Changes

--- src/Components/User/Home/Home.jsx
+++ src/Components/User/Home/Home.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import TopNavbar from '../TopNavbar/TopNavbar'
 import SideNavbar from '../SideNavbar/SideNavbar'
-import { useNavigate } from 'react-router-dom'
 
 
 function Home() {
-
-  
-  const navigate = useNavigate()
 
 
   return (
     <div>
       <TopNavbar/>
       <SideNavbar/>
-      <button onClick={() => {navigate("/admin")}}>admin</button>
     </div>
   )
 }
